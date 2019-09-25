bba257545c34880b20dacb0349311cbd8be49e6d
Vangelis <evlogaras@yahoo.com>
2019-09-25 15:58:05 +0300

    Comments added.

--- ModelASensorsRTCTest/ModelASensorsRTCTest.ino
+++ ModelASensorsRTCTest/ModelASensorsRTCTest.ino
@@ -1,3 +1,12 @@
+/**
+  Trackcol ModelA
+  Name: ModelASensorsRTCTest
+  Purpose: ESP32 plus sensors and RTC test code
+
+  @author E. Logaras
+  @version 1.0 25/09/2019
+*/
+
 void setup() {
   // put your setup code here, to run once:
 
